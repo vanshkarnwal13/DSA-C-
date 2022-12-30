2ddb660e4a57452d9832e32ac59f0ced76f6cf5b
Vansh Karnwal <vanshkarnwal@Vanshs-MacBook-Air.local>
2022-12-30 12:32:30 +0530

    update

---
+++ binary_tree.cpp
@@ -0,0 +1,184 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+template<typename T>
+class BinaryTreeNode{
+public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+    BinaryTreeNode(T data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTreeNode(){
+        delete left;
+        delete right;
+    }
+};
+BinaryTreeNode<int> *takeInput(){
+    int rootData;
+    cout << "Enter data: ";
+    cin >> rootData;
+    if(rootData == -1){
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    BinaryTreeNode<int> *leftChild = takeInput();
+    BinaryTreeNode<int> *rightChild = takeInput();
+    root->left = leftChild;
+    root->right = rightChild;
+    return root;
+}
+BinaryTreeNode<int> *takeInputLevelWise(){
+    int rootData;
+    cout << "Enter root data: ";
+    cin >> rootData;
+    if(rootData == -1){
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    while(pendingNodes.size() != 0){
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        int leftChildData;
+        cout << "Enter left child of " << front->data << ": ";
+        cin >> leftChildData;
+        if(leftChildData != -1){
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(leftChildData);
+            front->left = child;
+            pendingNodes.push(child);
+        }
+        int rightChildData;
+        cout << "Enter right child of " << front->data << ": ";
+        cin >> rightChildData;
+        if(rightChildData != -1){
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(rightChildData);
+            front->right = child;
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+void printTree(BinaryTreeNode<int> *root){
+    if(root == NULL){
+        return;
+    }
+    cout << root->data << ":";
+    if(root->left != NULL){
+        cout << "L" << root->left->data << ",";
+    }
+    if(root->right != NULL){
+        cout << "R" << root->right->data;
+    }
+    cout << endl;
+    printTree(root->left);
+    printTree(root->right);
+}
+void printTreeLevelwise(BinaryTreeNode<int> *root){
+    if(root == NULL){
+        return;
+    }
+    queue<BinaryTreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    while(pendingNodes.size() != 0){
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << front->data << ":";
+        if(front->left != NULL){
+            cout << "L" << front->left->data << ",";
+            pendingNodes.push(front->left);
+        }
+        if(front->right != NULL){
+            cout << "R" << front->right->data;
+            pendingNodes.push(front->right);
+        }
+        cout << endl;
+    }
+}
+int numNodes(BinaryTreeNode<int> *root){
+    if(root == NULL){
+        return 0;
+    }
+    return 1 + numNodes(root->left) + numNodes(root->right);
+} 
+void inorder(BinaryTreeNode<int> *root){
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+void preorder(BinaryTreeNode<int> *root){
+    if(root == NULL){
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(BinaryTreeNode<int> *root){
+    if(root == NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+BinaryTreeNode<int>* buildTreeHelper(int* in, int* pre, int inS, int inE, int preS, int preE){
+    if(inS > inE){
+        return NULL;
+    }
+    int rootData = pre[preS];
+    int rootIndex = -1;
+    for(int i=inS; i<=inE; i++){
+        if(in[i] == rootData){
+            rootIndex = i;
+            break;
+        }
+    }
+    int lInS = inS;
+    int lInE = rootIndex - 1;
+    int lPreS = preS + 1;
+    int lPreE = lInE - lInS + lPreS;
+    int rInS = rootIndex + 1;
+    int rInE = inE;
+    int rPreS = lPreE + 1;
+    int rPreE = preE;
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    root->left = buildTreeHelper(in, pre, lInS, lInE, lPreS, lPreE);
+    root->right = buildTreeHelper(in, pre, rInS, rInE, rPreS, rPreE);
+    return root;
+}
+BinaryTreeNode<int>* buildTree(int* in, int* pre, int size){
+    return buildTreeHelper(in, pre, 0, size-1, 0, size-1);
+}
+
+int main(){
+    // BinaryTreeNode<int> *root = new BinaryTreeNode<int>(1);
+    // BinaryTreeNode<int> *node1 = new BinaryTreeNode<int>(2);
+    // BinaryTreeNode<int> *node2 = new BinaryTreeNode<int>(3);
+    // root->left = node1;
+    // root->right = node2;
+    BinaryTreeNode<int> *root = takeInputLevelWise();
+    printTree(root);
+    printTreeLevelwise(root);
+    cout << "Number of nodes: " << numNodes(root) << endl;
+    cout << "Inorder: ";
+    inorder(root);
+    cout << endl;
+    cout << "Preorder: ";
+    preorder(root);
+    cout << endl;
+    cout << "Postorder: ";
+    postorder(root);
+    cout << endl;
+
+
+    delete root;
+    return 0; 
+}
--- tree.c++
+++ tree.c++
@@ -10,6 +10,11 @@ class Tree {
     Tree(T data) {
         this->data = data;
     }
+    ~Tree() {
+        for(int i=0;i<children.size();i++) {
+            delete children[i];
+        }
+    }
 
 };
 Tree<int>* takeInputLevelWise(){
@@ -50,6 +55,25 @@ Tree<int>* takeInput() {
     }
     return root;
 }
+void printTreeLevelWise(Tree<int>* root) {
+    queue<Tree<int>*> pendingNodes;
+    pendingNodes.push(root);
+    while(pendingNodes.size()!=0) {
+        Tree<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        cout<<front->data<<":";
+        for(int i=0;i<front->children.size();i++) {
+            if(i<front->children.size()-1) {
+                cout<<front->children[i]->data<<",";
+            }
+            else {
+                cout<<front->children[i]->data;
+            }
+            pendingNodes.push(front->children[i]);
+        }
+        cout<<endl;
+    }
+}
 void printTree(Tree<int>* root) {
     if(root == NULL) {
         return;
@@ -63,6 +87,66 @@ void printTree(Tree<int>* root) {
         printTree(root->children[i]);
     }
 }
+int numNode(Tree<int>* root) {
+    if(root == NULL) {
+        return 0;
+    }
+    int ans = 1;
+    for(int i=0;i<root->children.size();i++) {
+        ans = ans + numNode(root->children[i]);
+    }
+    return ans;
+}
+int height(Tree<int>* root) {
+    if(root == NULL) {
+        return 0;
+    }
+    int ans = 0;
+    for(int i=0;i<root->children.size();i++) {
+        int childHeight = height(root->children[i]);
+        if(childHeight > ans) {
+            ans = childHeight;
+        }
+    }
+    return ans+1;
+}
+void printAtLevelK(Tree<int>* root, int k) {
+    if(root == NULL) {
+        return;
+    }
+    if(k == 0) {
+        cout<<root->data<<endl;
+        return;
+    }
+    for(int i=0;i<root->children.size();i++) {
+        printAtLevelK(root->children[i], k-1);
+    }
+}
+void preoder(Tree<int>* root) {
+    if(root == NULL) {
+        return;
+    }
+    cout<<root->data<<" ";
+    for(int i=0;i<root->children.size();i++) {
+        preoder(root->children[i]);
+    }
+}
+void postorder(Tree<int>* root) {
+    if(root == NULL) {
+        return;
+    }
+    for(int i=0;i<root->children.size();i++) {
+        postorder(root->children[i]);
+    }
+    cout<<root->data<<" ";
+}
+void deleteTree(Tree<int>* root) {
+    for(int i=0;i<root->children.size();i++) {
+        deleteTree(root->children[i]);
+    }
+    delete root;
+}
+
 int main(){
     // Tree<int>* root = new Tree<int>(1);
     // Tree<int>* node1 = new Tree<int>(2);
@@ -74,6 +158,9 @@ int main(){
     // cout<<root->children[1]->data<<endl;
     Tree<int>* root = takeInputLevelWise();
     printTree(root);
+    printTreeLevelWise(root);
+    cout<<"Number of nodes in tree is "<<numNode(root)<<endl;
+
 
     return 0; 
 
